0c65c7149b7c643563cd0bd2e4d0b02e36fd3779
QuadOrange <jakekeech@gmail.com>
2016-07-29 17:25:54 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ $(function() {
     $("#getJoke").click(
         function() {
             $.get("/jokes",function(data){
-                // TODO:
-                // modify this code to manipulate the HTML
-                // to display the setup and punchline on the page
-                // instead of using alerts!
                 $("#setup").html(data.setup);
                 $("#punchline").html(data.punchline);
                 index = data.id;
